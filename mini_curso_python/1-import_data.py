5100e1963072d021a44b2663a58d9e0866491e4a
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-10-31 09:06:24 -0300

    funções da biblioteca pandas

--- mini_curso_python/1-import_data.py
+++ mini_curso_python/1-import_data.py
@@ -3,3 +3,12 @@ import pandas as pd
 # 1. Importando dados de um arquivo CSV
 data = pd.read_excel("data/VendaCarros.xlsx")
 print(data)
+
+#2. Listar primeiros registros
+print(data.head())
+
+#3. Listar últimos registros
+print(data.tail())
+
+#4. Contagem de valores por fabricante
+print(data['Fabricante'].value_counts())
